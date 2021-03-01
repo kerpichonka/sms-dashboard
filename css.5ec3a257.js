46f57d75a87996a340973c7169c00a20207bf1e1
Dima <dimontregyb@email.ua>
2021-03-02 00:23:57 +0200

    Updates

--- css.5ec3a257.js
+++ css.5ec3a257.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Projects\\sms-dashboard\\src\\assets\\img\\bgr-image.jpg":[["bgr-image.2dacab1a.jpg","assets/img/bgr-image.jpg"],"assets/img/bgr-image.jpg"],"./..\\fonts\\ProximaNova-Regular.woff2":[["ProximaNova-Regular.e55f39f7.woff2","assets/fonts/ProximaNova-Regular.woff2"],"assets/fonts/ProximaNova-Regular.woff2"],"./..\\fonts\\ProximaNova-Regular.woff":[["ProximaNova-Regular.d677b782.woff","assets/fonts/ProximaNova-Regular.woff"],"assets/fonts/ProximaNova-Regular.woff"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55916" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59652" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
