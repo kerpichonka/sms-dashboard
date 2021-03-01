c7a6fbdf2386672102d9a10fac24094550b63645
Dima <dimontregyb@email.ua>
2021-03-02 00:23:04 +0200

    create header & add bg-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: theme => ({
+                'image': "url('/assets/img/bgr-image.jpg')",
+               }),
+            spacing: {
+                '444': '444px',
+            },
+            margin: {
+                '43': '43px',
+            },
+            maxWidth: {
+                '1110': '1110px',
+            }
+        },
+        fontFamily: {
+            'body': ['Proxima Nova', 'sans-serif']
+        },
     },
     variants: {
         extend: {},
